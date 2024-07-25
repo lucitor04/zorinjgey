786f62ee505421a793e552ec3e818166b2fec685
lucitor <107627311+lucitor04@users.noreply.github.com>
2024-07-25 16:33:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,55 @@
-let values = [];
+           const generateButton = document.getElementById('generateButton');
+    const select2 = document.getElementById('select2');
+    const secretNumberElement = document.getElementById('secretNumber');
+    const guessButton = document.getElementById('guessButton');
+    const userInput = document.getElementById('userInput');
+    const resultsList = document.getElementById('resultsList');
+    let secretNumber = '';
 
-document.getElementById('addButton').addEventListener('click', () => {
-    const inputValue = document.getElementById('inputValue').value;
-    if (inputValue) {
-        values.push(+(inputValue));
-        document.getElementById('inputValue').value = '';
-    }
-});
-
-    document.getElementById('calculateButton').addEventListener('click', () => {
-    const outputDiv = document.getElementById('output');
-    if (values.length > 0) {
-        let max = values[0];
-        let min = values[0];
-        let sum = 0;
-        
-        for (let i = 0; i < values.length; i++) {
-            if (values[i] > max) {
-                max = values[i];
-            }
-            if (values[i] < min) {
-                min = values[i];
-            }
-            sum += values[i];
+    function generateSecretNumber(numDigits) {
+        let digits = '';
+        for (let i = 0; i < numDigits; i++) {
+            digits += Math.floor(Math.random() * 10).toString();
         }
-        outputDiv.innerHTML += `
-        <li>max: ${max}
-        min: ${min}
-        sum: ${sum}
-        ${values}</li>
-        `;
+        return digits;
     }
-});
+
+    generateButton.addEventListener('click', () => {
+        alert("da")
+        const numDigits = parseInt(select2.value);
+        secretNumber = generateSecretNumber(numDigits);
+        secretNumberElement.textContent = 'Число загадано! Пробуйте угадать!';
+        userInput.maxLength = numDigits;
+        alert(secretNumber);
+    });
+
+    guessButton.addEventListener('click', () => {
+        const userGuess = userInput.value;
+        if (userGuess.length === secretNumber.length) {
+            let bulls = 0;
+            let cows = 0;
+
+            for (let i = 0; i < secretNumber.length; i++) {
+                if (userGuess[i] === secretNumber[i]) {
+                    bulls++;
+                }
+            }
+
+            for (let i = 0; i < secretNumber.length; i++) {
+                for (let j = 0; j < secretNumber.length; j++) {
+                    if (i !== j && userGuess[i] === secretNumber[j]) {
+                        cows++;
+                        break;
+                    }
+                }
+            }
+
+            resultsList.innerHTML += `<li>${userGuess}: ${bulls} быков, ${cows} коров</li>`;
+            userInput.value = '';
+            if (userGuess === secretNumber) {
+                alert("Победа!");
+            }
+        } else {
+            alert(`Введите ${secretNumber.length} значное число.`);
+        }
+    });
